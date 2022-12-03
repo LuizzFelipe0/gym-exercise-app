212352c50d1f647ca9504bb520562b2b91d9c33e
Luiz Felipe Canário Costa <luizfelipecanariocosta@gmail.com>
2022-12-03 16:08:59 -0300

    initial commit

---
+++ gymApp/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { style } from './tailwindStyle';
+
+function App() {
+
+
+
+  return (
+    <div className={style.backg}>
+      <div className={style.container}>
+        <h1 className={style.heading}>Exercise++</h1>
+      </div>
+    </div>
+
+
+
+  );
+}
+
+export default App;
---
+++ gymApp/src/tailwindStyle.js
@@ -0,0 +1,25 @@
+export const style = {
+    
+    backg: `h-screen w-screen p-4 bg-gradient-to-t from-[#0D0B50] to-[#870000]` ,
+    container: `bg-black  max-w-[30%] max-h-[130px] w-full m-auto rounded-lg shadow-2xl shadow-zinc-900 my-10   `,
+    heading: `text-5xl font-bold text-center text-white	p-3 my-3 hover:text-gray duration-300 `,
+    form: `flex justify-start`,
+    input: `border-4 border-black rounded-md p-2 w-11/12 text-xl`,
+    button: `border-4 border-black p-2 ml-2 bg-white rounded-lg text-red-600 hover:text-red-700 duration-300 `,
+    count: `text-center p-2 font-bold` ,
+    author: `text-center mt-4 p-4 text-white `
+    
+};
+
+export const styleli = {
+
+    list: `border-4 border-black flex justify-between bg-violet-400 rounded-md p-4 my-4 capitalize `,
+    row: `flex`,
+    completeList: `border-4 border-black font-bold flex justify-between bg-green-500 rounded-md p-4 my-4 capitalize`,
+    text: `ml-2 cursor-pointer`,
+    completeText: `font-bold ml-2 cursor-pointer line-through`,
+    button: `cursor-pointer flex itens-center`,
+    checkbox: `cursor-pointer w-5 `
+    
+    };
+    
