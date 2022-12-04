d2cd857042e6e5d858f687b33141aea251aa74cd
Luiz Felipe Canário Costa <luizfelipecanariocosta@gmail.com>
2022-12-04 14:37:51 -0300

    working api

--- gymApp/src/App.js
+++ gymApp/src/App.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { style } from './tailwindStyle';
+import axios from 'axios';
 
-function App() {
+const App = (props) => {
 
+  const [exercises, setExercises] = useState([]);
 
-
+  const render = async () => {
+    const response = await axios.get(
+      "https://wger.de/api/v2/exercise"
+    );
+    setExercises(response.data.results);
+  };
+ 
+ 
+  useEffect(() => {
+    render();
+  }, []);
+  
+  
   return (
     <div className={style.backg}>
       <div className={style.container}>
-        <h1 className={style.heading}>Exercise++</h1>
+        <h1 className={style.heading}>Exercise ++</h1>
       </div>
+      {exercises.map(e => (
+        <div className={style.container}>
+        <li className={style.heading} key={e.id}>
+          
+            
+            {e.name}
+          
+        </li>
+        </div>
+      ))}
     </div>
 
 
 
   );
-}
+};
 
 export default App;
